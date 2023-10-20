372dc0d9aa1e8e8e11e3b211a6d968bb05291533
lavisar <sarlavi6969@gmail.com>
2023-10-20 16:18:16 +0700

    Generate JWT Backend

--- src/components/Code/Code.js
+++ src/components/Code/Code.js
@@ -1,15 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useSearchParams } from "react-router-dom";
 import axios from "axios";
 
 const Code = (props) => {
 	const [searhcParams, setSearchParams] = useSearchParams();
+	const firsRunRef = useRef(false);
 
 	useEffect(() => {
-		let ssoToken = searhcParams.get("ssoToken");
-		if (ssoToken) {
+		const ssoToken = searhcParams.get("ssoToken");
+		if (ssoToken && firsRunRef.current === false) {
+			firsRunRef.current = true;
 			axios
-				.post(process.env.REACT_APP_BACKEND_SSO_VERIFY_TOKEN, {ssoToken})
+				.post(
+					process.env.REACT_APP_BACKEND_SSO_VERIFY_TOKEN,
+					{ ssoToken },
+					{ withCredentials: true }
+				)
 				.then((res) => {
 					console.log(">>> res", res);
 				})
